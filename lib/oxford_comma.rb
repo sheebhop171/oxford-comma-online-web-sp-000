ca638549bd4b8a34a117373fb8269422d19b6dd1
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-21 18:55:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.length == 1
     array.join
-  else array.length == 2
+  elsif array.length == 2
     array.join(" and ")
+  elsif array.length == 3
+    array.join(",")
   end
 end
